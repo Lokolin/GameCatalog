9ef74a433502d2a067b9e66028e470eef8ecd4ce
Дмитрий Евсюков <evsukovdmitry@macbook-air-dmitrij.local>
2018-10-23 21:14:26 +0300

    Initial Commit

---
+++ GameCatalogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCatalogs
+//
+//  Created by Дмитрий Евсюков on 23/10/2018.
+//  Copyright © 2018 Дмитрий Евсюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
